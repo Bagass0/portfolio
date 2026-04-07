fce5b1adf09f2d1a7ce08530491de1a9304ab9fc
Hugo <80489889+Bagass0@users.noreply.github.com>
2026-04-07 11:10:29 +0200

    Update PageNotFound.jsx

--- Frontend/src/pages/PageNotFound.jsx
+++ Frontend/src/pages/PageNotFound.jsx
@@ -10,7 +10,7 @@ function PageNotFound() {
 
   return (
     <div className="notfound-container">
-      <div className="notfound-title">404</div>
+      <div className="notfound-title">404 lubulule</div>
       <div className="notfound-subtitle">{ textes.title }</div>
       <div className="notfound-text">{ textes.subtitle }</div>
       <Link to="/" className="notfound-link">{ textes.backToHome }</Link>
