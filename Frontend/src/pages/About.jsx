881e82c83d7022d280c1d7a92959c37540c0c176
Hugo <80489889+Bagass0@users.noreply.github.com>
2026-04-07 11:44:11 +0200

    A propos

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,19 +1,8 @@
 import '../styles/About.css';
-import { useContext } from 'react';
-import { ConfigContext } from '../context/ConfigContext';
-import { AboutTextes } from '../utils/textes';
 
 const About = () => {
-  const { lang } = useContext(ConfigContext);
-  const textes = AboutTextes[lang];
-
   return (
     <div className="projects-page">
-      <div className="dev-banner">
-        <span className="dev-emoji">🚧</span>
-        <span>{ textes.description }</span>
-        <span className="dev-emoji">🚧</span>
-      </div>
       <div className="about-hosting-section">
         <h2>À propos de ce portfolio</h2>
         <p>
